3d263b2007940575a656db691c00aca28896c4a1
Vhs0808 <schmittvitorhugo2@gmail.com>
2026-09-04 20:46:26 -0300

    fix: Ajusta tipo de mapping feito nos métodos da calculadora

--- src/main/java/com/schmitt/calculadora_spring/controller/CalculadoraController.java
+++ src/main/java/com/schmitt/calculadora_spring/controller/CalculadoraController.java
@@ -3,9 +3,9 @@ package com.schmitt.calculadora_spring.controller;
 import com.schmitt.calculadora_spring.dto.CalculoResponseDto;
 import com.schmitt.calculadora_spring.dto.ResponseDto;
 import com.schmitt.calculadora_spring.dto.ValoresRequestDto;
-import com.schmitt.calculadora_spring.excessoes.DivisaoPorZeroException;
 import com.schmitt.calculadora_spring.service.CalculadoraService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,22 +19,22 @@ public class CalculadoraController {
     @Autowired
     ResponseDto response;
 
-    @RequestMapping("/soma")
+    @PostMapping("/soma")
     public CalculoResponseDto soma(@RequestBody ValoresRequestDto valores){
         return calculadora.soma(valores);
     }
 
-    @RequestMapping("/subtracao")
+    @PostMapping("/subtracao")
     public CalculoResponseDto subtracao(@RequestBody ValoresRequestDto valores){
         return calculadora.subtracao(valores);
     }
 
-    @RequestMapping("/multiplicacao")
+    @PostMapping("/multiplicacao")
     public CalculoResponseDto multiplicacao(@RequestBody ValoresRequestDto valores) {
         return calculadora.multiplicacao(valores);
     }
 
-    @RequestMapping("/divisao")
+    @PostMapping("/divisao")
     public CalculoResponseDto divisao(@RequestBody ValoresRequestDto valores) {
         return calculadora.divisao(valores);
     }
