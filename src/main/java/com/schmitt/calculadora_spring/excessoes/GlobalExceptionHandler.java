acb8cc6b0e52e74f89847dd2eed2536c318a9e3f
Vhs0808 <schmittvitorhugo2@gmail.com>
2026-09-04 20:45:56 -0300

    fix: Muda Global Hendler de diretório

--- src/main/java/com/schmitt/calculadora_spring/excessoes/GlobalExceptionHandler.java
+++ src/main/java/com/schmitt/calculadora_spring/excessoes/GlobalExceptionHandler.java
@@ -1,7 +1,6 @@
-package com.schmitt.calculadora_spring.controller;
+package com.schmitt.calculadora_spring.excessoes;
 
 import com.schmitt.calculadora_spring.dto.ResponseDto;
-import com.schmitt.calculadora_spring.excessoes.DivisaoPorZeroException;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
